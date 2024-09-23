98317f670a4391c8b1fe00db71746250ead181f3
Tristan de Saint Gilles <tristan.de_saint_gilles@edu.univ-fcomte.fr>
2024-09-23 16:45:22 +0200

    Edge API, CompareTo

--- m1graphs2024/Edge.java
+++ m1graphs2024/Edge.java
@@ -1,10 +1,13 @@
-import java.lang.RuntimeException;
+import java.util.Objects;
 
 /**
  * The class Edge aims at physically coding edges as object
  * @author Tristan de Saint Gilles
  */
 public class Edge implements Comparable<Edge>{
+
+    /* Attributes */
+    
     /**
      * The node from where the edge starts
      */
@@ -25,6 +28,8 @@ public class Edge implements Comparable<Edge>{
      * 
      */
     private Graph graphHolder;
+
+    /* Constructors */
 
     /**
      * Constructor for a non weighted Edge
@@ -84,8 +89,9 @@ public class Edge implements Comparable<Edge>{
      * @param from the Node where it begins
      * @param to the Node where it ends
      * @param weight the weight of the Edge
-     * @throws IllegalArgumentException if any of the arguments are null 
-     * or if the graph of @from is different from the one of @to
+     * @throws IllegalArgumentException 
+     * if any of the arguments are null or
+     * if the graph of @from is different from the one of @to
      */
     Edge(Graph graphHolder, int from, int to, int weight) {
         if(graphHolder == null || graphHolder.getNode(from) == null || 
@@ -98,6 +104,34 @@ public class Edge implements Comparable<Edge>{
         this.weight = weight;
     }
 
+    /* API */
+
+    public Node from(){
+        return from;
+    }
+
+    public Node to(){
+        return to;
+    }
+
+    public Edge getSymmetric(){
+        return new Edge(this.graphHolder, this.to, this.from);
+    }
+
+    public boolean isSelfLoop(){
+        return this.from.equals(this.to);
+    }
+
+    public boolean isWeighted(){
+        return weight != null;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    /* Overrides */
+
     /**
      * Check if current Edge is equal to another object
      * @param obj another object
@@ -109,12 +143,8 @@ public class Edge implements Comparable<Edge>{
         
         Edge other = (Edge) obj;
 
-        if (from == other.from && to == other.to  && 
-        ((weight == null && other.weight == null) || weight == other.weight)) {
-            return true;
-        }
-
-        return false;
+        return from == other.from && to == other.to  && 
+                ((weight == null && other.weight == null) || Objects.equals(weight, other.weight));
     }
 
     /**
@@ -122,8 +152,11 @@ public class Edge implements Comparable<Edge>{
      */
     @Override
     public int hashCode() {
-        // TODO Auto-generated method stub
-        return super.hashCode();
+        int hash = 5;
+        hash = 31 * hash * (this.from != null ? this.from.hashCode() : 0);
+        hash = 31 * hash * (this.to != null ? this.to.hashCode() : 0);
+        hash = 31 * hash * (this.weight != null ? this.weight : 0);
+        return hash;
     }
 
     /**
@@ -133,9 +166,9 @@ public class Edge implements Comparable<Edge>{
      */
 	@Override
 	public int compareTo(Edge o) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+        if (from.getId() != o.from.getId()) return from.getId() - o.from.getId();
+        if (to.getId() != o.to.getId()) return to.getId() - o.to.getId(); 
+        if (weight != null && o.weight != null && weight != o.weight) return weight - o.weight;
+        return 0;
 	}
-
-    // TODO Implement a Comparable<Edge> interface
 }
