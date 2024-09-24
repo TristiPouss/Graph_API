d0cb2998e86bc155aa2af2e5c6f33fb00a2a77e4
Tristan de Saint Gilles <tristan.de_saint_gilles@edu.univ-fcomte.fr>
2024-09-24 09:50:18 +0200

    graph constructor, methods

--- m1graphs2024/Graph.java
+++ m1graphs2024/Graph.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 public class Graph {
+
+    private Map<Node, List<Edge>> adjEdList;
+
+    public Graph() {
+        adjEdList = new HashMap<>();
+    }
+
+    /**
+     * @return the number of nodes in the graph
+     */
     public int nbNodes(){
-        return 1;
+        return adjEdList.size();
+    }
+
+    /**
+     * 
+     * @param i an integer
+     * @return the node with the id corresponding parameter @i
+     */
+    Node getNode(int i) {
+        /*if (adjEdList.keySet().contains(i)){
+            
+        }*/
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
