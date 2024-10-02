111b29a7b6964ce403cb8f5e0fda52b8921d1f03
Tristan de Saint Gilles <tristan.de_saint_gilles@edu.univ-fcomte.fr>
2024-10-02 17:58:40 +0200

    Undirected

--- m1graphs2024/UndirectedGraph.java
+++ m1graphs2024/UndirectedGraph.java
@@ -1,3 +1,38 @@
+
+import java.util.*;
+
 public class UndirectedGraph extends Graph{
+    /* Attributes */
+
+    /*
+     * The Adjencent Edge List implemented as a Map
+     */
+    private Map<Node, List<Edge>> adjEdList;
+
+    /* Constructors */
+
+    /**
+     * Constructor for empty graph
+     */
+    public UndirectedGraph() {
+        super();
+    }
     
+    /**
+     * Constructor from Successor array
+     * @param sa unknown number of int as successor array
+     */
+    public UndirectedGraph(int ... sa) {
+        super(sa);
+    }
+
+    @Override
+    public UndirectedGraph getTransitiveClosure() {
+        return (UndirectedGraph) super.getTransitiveClosure();
+    }
+
+    /*@Override
+    public static UndirectedGraph fromDotFile(String filename) {
+        return (UndirectedGraph) super.fromDotFile(filename);
+    }*/
 }
