85b912fdd39e82d8452320d48a7be74fcf4723c9
Tristan de Saint Gilles <tristan.de_saint_gilles@edu.univ-fcomte.fr>
2024-10-26 15:07:56 +0200

    test dfs

--- m1graphs2024/TestGraphsPW2.java
+++ m1graphs2024/TestGraphsPW2.java
@@ -8,11 +8,21 @@ import java.util.List;
 public class TestGraphsPW2 {
 
 	public static void main(String[] args) throws IOException {
+		//Graph test = new Graph(2, 0, 3, 0, 5, 0, 1, 3, 6, 0, 6, 7, 8, 0, 9, 0, 9, 0, 9);
+		Graph test = new Graph(2, 3, 4, 0, 4, 0, 5, 0, 6, 7, 0, 6, 8, 0, 3, 0, 0, 0);
+		test.toDotFile("./test/firstGraph", ".dot");
+		System.out.print("DFS : [");
+		for (Node n : test.getDFS()) {
+			System.out.print(n.toString() + " ");
+		}
+		System.out.print("]\n");
+		//test.getTransitiveClosure().toDotFile("./test/firstGraph_TransClosure", ".dot");
+		//
+
 		System.out.println("************ PART 1. UNWEIGTED DIRECTED GRAPHS ***********************");
 		System.out.println("\n>>>>>>>> SIMPLE GRAPH >>>>>>>>>>>>>>>>>>>>>>>>");
 		System.out.println(">>>>>>>> Creating the subject example graph in G");
 		Graph g = new Graph(2, 4, 0, 0, 6, 0, 2, 3, 5, 8, 0, 0, 4, 7, 0, 3, 0, 7, 0);
-		g.toDotFile("./test/firstGraph");
 		System.out.println(">>>> Graph information");
 		System.out.println(">> DOT representation\n"+g.toDotString());
 		System.out.println(""+g.nbNodes()+" nodes, "+g.nbEdges()+" edges");
