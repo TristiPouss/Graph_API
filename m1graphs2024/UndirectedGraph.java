353283ad4e03f1455547c1c73b38030a64fcd4f1
rjoffrin <renaud.joffrin@edu.univ-fcomte.fr>
2024-11-05 20:06:11 +0100

    fromDotFile for undirectedGraph

--- m1graphs2024/UndirectedGraph.java
+++ m1graphs2024/UndirectedGraph.java
@@ -1,14 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
-<<<<<<< HEAD
- * The class UndirectedGraph is a representation of a undirected graph with an Adjacency Edge list
-=======
  * An extend of the class Graph to represent an undirected graph
->>>>>>> 23af37922c755f540ec8465cb85e9540fa6ee285
  * @author Tristan de Saint Gilles
  * @author Renaud Joffrin
  */
@@ -21,6 +21,15 @@ public class UndirectedGraph extends Graph{
      */
     public UndirectedGraph() {
         super();
+    }
+
+    /**
+     * Constructor for empty graph with a name
+     * 
+     * @param name The name for the graph
+     */
+    public UndirectedGraph(String name) {
+        super(name);
     }
     
     /**
@@ -524,10 +533,78 @@ public class UndirectedGraph extends Graph{
     }
 
     public static UndirectedGraph fromDotFile(String filename) {
-        return (UndirectedGraph) Graph.fromDotFile(filename);
+        return fromDotFile(filename, ".gv");
     }
 
+    /**
+      * for importing a file in the restricted DOT format
+      * @param filename a String. The absolute path to the DOT file with no extension
+      * @param extension a String, The extension of the file
+      * @return a Graph
+      */
     public static UndirectedGraph fromDotFile(String filename, String extension) {
-        return null;
+        if(!(extension.equals(".gv") || extension.equals(".dot"))){
+            return null;
+        }
+        UndirectedGraph result = null;
+        File newFile = new File("./m1graphs2024/dotGraphsTestPW2/" + filename + extension);
+        try{
+            Scanner parser = new Scanner(newFile);
+            while(parser.hasNextLine()){
+                String curr = parser.nextLine().trim();
+
+                if(curr.charAt(0) == '#' || curr.isEmpty()){
+                    continue;
+                }
+
+                String[] token = curr.split("\\s+");
+                if(curr.contains("{")){
+                    if(token.length == 3){
+                        if(Objects.equals(token[2], "{")){
+                            if(token[0].equals("graph")){
+                                result = new UndirectedGraph(token[1]);
+                            }else{
+                                return null;
+                            }
+                        }
+                    }else{
+                        if(token[0].equals("graph")){
+                            result = new UndirectedGraph();
+                        }else{
+                            return null;
+                        }
+                    }
+                }
+
+                if(token[token.length - 1].equals("}")){
+                    return result;
+                }
+                
+                if(result != null){
+                    if(token.length >= 3){
+                        if(token[1].equals("--")){
+                            int node1 = Integer.parseInt(token[0]);
+                            int node2 = Integer.parseInt(token[2]);
+                            result.addNode(node1);
+                            result.addNode(node2);
+
+                            if(token.length > 3){
+                                result.addEdge(node1, node2, Integer.parseInt(token[token.length - 1].split("=")[1].replace("]", "")));
+                            }else{
+                                result.addEdge(node1, node2);
+                            }
+                        }
+                    }else{
+                        if(token.length == 1 && token[0].matches("[0-9]+")){
+                            result.addNode(Integer.parseInt(token[0]));
+                        }
+                    }
+                }
+            }
+        }catch(FileNotFoundException f){
+            throw new RuntimeException(f);
+        }
+        return result;
     }
+
 }
